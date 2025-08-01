d54b5874f3744257df4a2648b7ce11a1aad6bab3
Talha <64659821+talhaozbek61@users.noreply.github.com>
2025-08-01 15:25:41 +0300

    chore: install project dependencies

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="min-h-full flex items-center justify-center text-2xl font-bold text-primary">
+      Multi-Step Form with TanStack Form
+    </h1>
+  );
+}
